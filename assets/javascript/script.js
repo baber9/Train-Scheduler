11872da633ca49b1bdf016b6967be679a25bc064
Bryan Aber <baber9@gmail.com>
2018-03-20 13:01:00 -0500

    finished train update functionality, added train img, now highlighting trains leaving within 10 mins, complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,6 +18,24 @@ var TrainStation = {
     // METHOD - adds new trains to index.html schedule
     trainAdded: firebase.database().ref("trains/").on("child_added", function (snapshot) {
 
+        // Grab frequency
+        var tFrequency = snapshot.child("frequency").val();
+
+        // Grab firstTrain (time)
+        var firstTrainTime = snapshot.child("firstTrain").val();
+
+        // Calc minutes until arrival (call Method)
+        var timeUntilArrival = TrainStation.tMinutesUntilArrival(firstTrainTime, tFrequency);
+
+        // Calc next train arrival (call Method)
+        var nextTrainTime = TrainStation.nextTrainArrivalTime(timeUntilArrival);
+
+        // Update next train arrival and minutes until arrival
+        snapshot.ref.update ({
+            nextArrival: nextTrainTime,
+            nextArrivalMins: timeUntilArrival
+        })
+
         // create TR and TD for Key only
         var trDiv = $("<tr>");
         var tdDivKey = $("<td>");
@@ -26,14 +44,25 @@ var TrainStation = {
         tdDivKey.append(snapshot.key);
         trDiv.append(tdDivKey);
 
-        // Add remaining train details in ForEach Loop
+
+        // Add remaining train details in ForEach Loop, except for firstTrain
         snapshot.forEach(function(childSnapshot) {
-            var tdDiv = $("<td>");
-            tdDiv.append(childSnapshot.val());
-            trDiv.append(tdDiv);
+            if (childSnapshot.key != "firstTrain") {
+                var tdDiv = $("<td>");
+                tdDiv.append(childSnapshot.val());
+                trDiv.append(tdDiv);
+
+                // If Minutes to Arrival < 10, highlight in red
+                if(childSnapshot.key === "nextArrivalMins" && childSnapshot.val() < 10) {
+                    trDiv.addClass("danger");
+                } else {
+                    trDiv.removeClass("danger");
+                }
+            }
         })
         // Append new train to table#train-info
         $("#train-info").append(trDiv);
+
     }),
 
     // METHOD calculates time until arrival of next train (in mins)
@@ -66,20 +95,28 @@ var TrainStation = {
     },
 
     // METHOD to add train to index.html and firebase
+
+    // ONLY ADD TRAIN, CALCULATIONS IN CHILD_ADDED
     addTrain: function (trainName, destination, firstTrainTime, tFrequency) {
         
         var validTrainTime = this.validateTrainTime(firstTrainTime);
         var validFrequency = this.validateFrequency(tFrequency);
         if (validTrainTime && validFrequency) {
+
+            // SHOULD MOVE THIS TO CHILD_ADDED--------
+
             // Call method tMinutes Until Arrival
             var timeUntilArrival = this.tMinutesUntilArrival(firstTrainTime, tFrequency);
             // Calls method next Train Arrival Time
             var nextTrainTime = this.nextTrainArrivalTime(timeUntilArrival);
 
+            // --------------------------------------
+
             // Firebase Add
             this.database.ref("trains/" + trainName).set ({
                 destination: destination,
                 frequency: tFrequency,
+                firstTrain: firstTrainTime,
                 nextArrival: nextTrainTime,
                 nextArrivalMins: timeUntilArrival
             })
@@ -93,14 +130,27 @@ var TrainStation = {
         $("#destination-input").val(""),
         $("#first-train-time-input").val(""),
         $("#frequency-input").val("")
-        $("#first-train").removeClass("has-error");
-        $("#frequency").removeClass("has-error");
     },
 
+    // METHOD to validate FirstTrainTime form entry
     validateTrainTime: function (ftrain) {
         var ftrainHours = ftrain.substr(0, ftrain.indexOf(":"));
         var ftrainMins = ftrain.slice(-2);
-        if (ftrain === "" || ftrain.length > 5 || ftrain.length < 4 ||ftrain.indexOf(":") === -1 || ftrain.slice(-3, -2) != ":" ||isNaN(ftrainHours) || isNaN(ftrainMins) || ftrainHours > 23 || ftrainHours < 0 || ftrainMins > 59 || ftrainMins < 0) {
+        if (ftrain === "" 
+          || ftrain.length > 5 
+          || ftrain.length < 4
+          // ':' doesn't exist 
+          || ftrain.indexOf(":") === -1
+          // the 3rd char from the last is not a ':' 
+          || ftrain.slice(-3, -2) != ":" 
+          // the hours is NaN
+          || isNaN(ftrainHours)
+          // the mins is NaN 
+          || isNaN(ftrainMins) 
+          || ftrainHours > 23 
+          || ftrainHours < 0 
+          || ftrainMins > 59 
+          || ftrainMins < 0) {
             $("#first-train").addClass("has-error");
             return false;
         } else {
@@ -109,8 +159,11 @@ var TrainStation = {
         }
     },
 
+    // METHOD to validate Frequency form entry
     validateFrequency: function (freq) {
-        if (isNaN (freq) || freq < 1 || freq > 10080) {
+        if (isNaN (freq) 
+          || freq < 1 
+          || freq > 10080) {
             $("#frequency").addClass("has-error");
             return false;
         } else {
@@ -120,7 +173,7 @@ var TrainStation = {
     }
 }
 
-
+// This on-click function will start the addTrain method from the TrainStation Object
 $("#add-train").on("click", function(e) {
     e.preventDefault();
     TrainStation.addTrain(
@@ -130,10 +183,12 @@ $("#add-train").on("click", function(e) {
         $("#frequency-input").val())
 });
 
+// ONLOAD UPDATE TRAIN TIMES, CONTINUE EVERY MIN???
+    // CREATE A NEW METHOD CALLED UPDATE TIMES
+// USER INTERVALS FROM DAY 9 ACTIVITIES
+// MAKE TR A DIFF COLOR WHEN LESS THAN 10 MINS
 
-
-
-
+// MOVE CALCULATIONS INTO 'CHILD-ADDED'
 
 
 
